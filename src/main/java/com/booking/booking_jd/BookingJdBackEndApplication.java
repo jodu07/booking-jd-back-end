61c42bb2d70d09e37877745bb561555caf5376bb
jodu07 <jhonnat.dussan@gmail.com>
2025-06-20 01:22:14 +0200

    Initial commit: Spring Boot Booking-JD project

---
+++ src/main/java/com/booking/booking_jd/BookingJdBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.booking.booking_jd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingJdBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingJdBackEndApplication.class, args);
+	}
+
+}

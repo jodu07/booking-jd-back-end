2198cd42b7419b46fee33f82c2308d4db09980ad
jodu07 <jhonnat.dussan@gmail.com>
2025-06-22 17:11:14 +0200

    adjustment in the database connection

---
+++ src/main/java/com/bookingjd/booking/model/BookingJdBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.bookingjd.booking.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingJdBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingJdBackEndApplication.class, args);
+	}
+
+}

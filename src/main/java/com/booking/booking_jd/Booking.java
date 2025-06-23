1f3989323bc6a030b04380df61c8bc21f9969171
jodu07 <jhonnat.dussan@gmail.com>
2025-06-23 17:16:46 +0200

    Packet routing adjustment

---
+++ src/main/java/com/booking/booking_jd/Booking.java
@@ -0,0 +1,31 @@
+package com.booking.booking_jd;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Booking {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private LocalDate bookingDate;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    // Getters y Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public LocalDate getBookingDate() { return bookingDate; }
+    public void setBookingDate(LocalDate bookingDate) { this.bookingDate = bookingDate; }
+
+    public Customer getCustomer() { return customer; }
+    public void setCustomer(Customer customer) { this.customer = customer; }
+}

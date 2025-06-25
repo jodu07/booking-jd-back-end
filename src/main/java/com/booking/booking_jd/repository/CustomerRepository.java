cc23227e4e500620e9e58c0ff64eea979c7f68ab
jodu07 <jhonnat.dussan@gmail.com>
2025-06-25 19:50:14 +0200

    Customer and Booking repository created

---
+++ src/main/java/com/booking/booking_jd/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.booking.booking_jd.repository;
+import com.booking.booking_jd.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
+
+}
+
+
